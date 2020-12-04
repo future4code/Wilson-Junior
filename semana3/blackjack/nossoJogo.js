d8f36e2fe294e8a87191aff75528772ec59cf649
willfjr <willferreirajr@icloud.com>
2020-12-04 16:19:58 -0300

    Projeto da Semana 03

---
+++ semana3/blackjack/nossoJogo.js
@@ -0,0 +1,47 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+// Greeting Message
+
+const Oi = alert('Bem vindo ao jogo de Blackjack!')
+
+if(confirm("Quer iniciar uma nova rodada?")) {
+
+   const carta = comprarCarta(); 
+   
+   const Usuario = []
+   
+
+   console.log('essa é a mão do usuário', )
+
+   comprarCarta();
+   const Computador = [];
+   
+   console.log('essa é a mão do computador', )
+
+
+// console.log(carta.texto) 
+
+// console.log(carta.valor)
+  
+}   
+
+else {
+	let bye = console.log('O jogoacabou =(')
+}
+
+
+
+
+
+
