42abdfd117ca79c91f305c07d74dd6633ff6d99d
willfjr <willferreirajr@icloud.com>
2020-12-07 12:47:48 -0300

    Realizei os exercícios de interpretação de código

---
+++ semana4/funções/index.js
@@ -0,0 +1,53 @@
+//-----------------------------Exercícios de interpretação de código--------------------------------------------------//
+
+// EXERCÍCIO 01
+
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// console.log(minhaFuncao(2))
+// console.log(minhaFuncao(10))
+
+// 01.a) O console irá imprimir a multiplicação do valor atribuído no parâmetros inclusos, ou seja, 10 e 50 que são
+// o resultado da multiplicação por 5. 
+// 
+// b) Se fossem retirados os dois console.log e apenas fizessemos o chamado da função possivelmente será impresso no
+// console o valor da última multiplicação (50).
+
+// EXERCÍCIO 02 
+// let arrayDeNomes = ["Amanda", "Caio"];
+
+// const outraFuncao = function(array) {
+// 	for (let i = 0; i < 2; i++) {
+// 		console.log(array[i])
+// 	}
+// }
+
+// outraFuncao(arrayDeNomes);
+
+// 2. a) As saídas que serão impressas no console serão apenas os dois primeiros elementos da variável arrayDeNomes.
+// b) Serão impressos apenas esses dois nomes, Amanda e Caio.
+
+// EXERCÍCIO 03
+
+// const metodo = (array) => {
+//     let arrayFinal = [];
+  
+//     for (let x of array) {
+//           if (x % 2 === 0) {
+//           arrayFinal.push(x * x)
+//           }
+//     }
+  
+//     return arrayFinal;
+//   }
+
+
+//   A presente função está criando um array vazio que será preenchido por números que tenham o resto da divisão
+//   por 2, ou seja, números pares, incluídos no arrayFinal multiplicados por eles mesmo. O melhor nome seria: criaArrayNumsPar²
+
+//-----------------------------Exercícios de escrita de código--------------------------------------------------//
+
+// EXERCÍCIO 04
+
