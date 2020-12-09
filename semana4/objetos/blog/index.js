4cd74a96f7f123e73dca6d21c14c4f4e5f90aa87
willfjr <willferreirajr@icloud.com>
2020-12-09 16:43:54 -0300

    Meu primeiro código não subiu. Esse era o teste que eu estava tentando mudar umas coisas, mas infelizmente deu errado. Vou ter que revisar.

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,5 +1,63 @@
-var title = document.getElementById(titulo-post); 
 
-var author = document.getElementById(autor-post);
 
-var contentPost = document.getElementById(conteudo-post);
+function salvaTudo(){
+
+    var titulo = document.getElementById("titulo-post");
+    let userTitulo = [titulo.value];
+
+    var autor = document.getElementById("autor-post");
+    let userAutor = [autor.value];
+
+    var conteudo = document.getElementById("conteudo-post");
+    let userConteudo = [conteudo.value];
+    
+    // Emite um alerta se o usuário deixar em branco
+    if(userTitulo.length == ''  | userAutor.length == '' | userConteudo.length  == ''){
+        
+        alert('Você deixou algum campo em branco!');
+        
+        return false
+    }
+
+    //Salva o conteúdo 
+    else {
+    
+    var titulo = document.getElementById("titulo-post");
+    let userTitulo = [titulo.value];
+
+    var autor = document.getElementById("autor-post");
+    let userAutor = [autor.value];
+
+    var conteudo = document.getElementById("conteudo-post");
+    let userConteudo = [conteudo.value];
+
+
+        var text = {
+            title: userTitulo,
+            author: userAutor,
+            content:userConteudo
+        }
+        
+        var i;
+        
+        for (i = 0; i < userTitulo.length ;i++) {
+        text += userTitulo.elements[i].value + "<br>";
+        }
+
+        document.getElementById("container-de-posts").innerHTML = text.title;
+
+        for (i = 0; i < userAutor.length ;i++) {
+            text += userAutor.elements[i].value + "<br>";
+        }
+        
+        document.getElementById("container-de-posts").innerHTML = text.author;
+        
+        for (i = 0; i < userConteudo.length ;i++) {
+            text += userConteudo.elements[i].value + "<br>";
+        }  
+
+        document.getElementById("container-de-posts").innerHTML = text.content;
+    }        
+
+}
+
