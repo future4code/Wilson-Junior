ed37001ac5e33ed22c2bd662012b3253dd5cbd14
willfjr <willferreirajr@icloud.com>
2020-12-11 13:00:26 -0300

    Exercícios de interpretação de código finalizados.

---
+++ semana4/lista-de-exercicios/lista.js
@@ -0,0 +1,53 @@
+// ----------------------------- Exercícios de interpretação de código -----------------------------//
+
+// EXERCÍCIO 01
+
+    /* Esse código irá realizar a conversão de um valor em dólar para reais. O usuário inserirá o valor de cotação 
+no prompt que surgirá para ele requerendo o valor de cotação. No exemplo, considerando que o valor do parâmetro inserido
+ na função foi de 100 dólares, o resultado final será 100 vezes o valor da cotação fornecida pelo usuário que saberá quanto
+ dinheiro tem em reais através do console.*/
+
+// EXERCÍCIO 02
+
+    /* Trata-se de um programa que mostrará no console os valores após um dado investimento. A função apresenta dois 
+ parâmetros 'tipoDeInvestimento' e 'valor', e, após passar por casos possíveis, retorna o valor após o investimento.
+ É uma espécie de calculadora utilizando o switch como fator que estabelece a condicional. Se o usuário escolhe saber
+ os rendimentos da Poupança, Renda Fixa, CDB ou Ações, bastará inserir o tipo de investimento desejado seguido do valor
+ a ser investido e o programa mostrará o resultado. No exemplo dado, O primeiro valor a ser impresso no console será
+ que o investimento foi em ações 'trasnformará' o valor inserido em 165. E o segundo montante, investido em tesouro
+ direto imprimirá um alert para o usuário informando que a opção tesouro direto é incorreta.*/
+
+// EXERCÍCIO 03
+
+    /* O programa analisa e seleciona os números existentes na constante números que é um array. A seleção ocorre na função for
+ que analisa se o número é divisível por 2 com resto 0, ou seja, par. Caso fique configurado que o número tem resto da divisão
+ por 2 igual a zero ele será inserido na constante array1, caso seja ímpar, o número será inserido no array2. Por fim, 
+ o console irá exibir apenas o tamanho dos arrays, ou seja, quantos elementos existem no array numeros, quantos foram incluídos
+ no array1 (ou seja, pares) e quantos foram incluídos no array2 (ou seja, ímmpares) */
+
+// EXERCÍCIO 04
+
+    /* O programa define três variáveis, uma definida com x números e outras duas com propriedades de infinitos números e
+ e outra igual a zero. A função for analisará cada número do array numeros e, caso o número não seja parte do conjunto dos
+ números infinitos e não nulos ele será parte da variável numero1. Se o número for acima de 0 será parte da variável numero2.
+ o console irá imprimir o valor da variável numero1 igual a -10 e para a variável numero2 o valor 1590 que é o maior número
+ do array de números.*/
+
+
+// ----------------------------- Exercícios de Lógica de Programação ------------------------------//
+
+
+
+
+// ----------------------------------- Exercícios de Funções --------------------------------------//
+
+
+
+
+// ----------------------------------- Exercícios de Objetos --------------------------------------//
+
+
+
+
+
+// ------------------------------- Exercícios de Funções de Array ---------------------------------//
