2c753d7cecf2fbc03af4a649967ada66e9c19558
willfjr <willferreirajr@icloud.com>
2020-12-16 17:04:25 -0300

    Exércícios realizados

---
+++ semana5/insta4/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import Post from './components/Post/Post';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={'app-container'}>
+        <Post
+          nomeUsuario={'Ester'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'Davi '}
+          fotoUsuario={'https://picsum.photos/50/51'}
+          fotoPost={'https://picsum.photos/200/151'}
+        />
+
+        <Post
+          nomeUsuario={'Theo'}
+          fotoUsuario={'https://picsum.photos/50/52'}
+          fotoPost={'https://picsum.photos/200/152'}
+        />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
