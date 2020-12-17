f26e8f83f2b6c24ef86fdeba9cecf2d84423be96
willfjr <willferreirajr@icloud.com>
2020-12-17 18:25:41 -0300

    Primeira parte dos exercícios realizada. Vou trabalhar na incluisão de novos posts.

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,27 +3,64 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
-  render() {
-    return (
+
+    state = {
+      userPost: [
+        {
+          nome: 'Ester',
+          foto: 'https://picsum.photos/50/50',
+          fotoPost:'https://picsum.photos/200/150'
+        },
+    
+        {
+          nome:'Davi ',
+          foto:'https://picsum.photos/50/51',
+          fotoPost:'https://picsum.photos/200/151'
+        },
+    
+        {
+          nome:'Theo',
+          foto:'https://picsum.photos/50/52',
+          fotoPost:'https://picsum.photos/200/152'
+        }
+      ],
+
+      inputNome:"",
+      inputFoto:"",
+      inputFotoPost:""
+
+    };
+
+  
+
+    adicionaPost = () => {
+      // Variável do novo post
+      const inputPost = {
+      // Capta o valor do inputNome
+        nome: this.state.inputNome,
+      // Capta o valor do inputFoto
+        foto: this.state.inputFoto,
+      // Capta o valor do inputFatoPost
+        fotoPost: this.state.inputFotoPost
+      };
+
+      // Faz a cópia do array principal e cria um elemento novo
+      const newPost = [...this.state.userPost, inputPost];
+ 
+
+render() {
+
+  const userPost = this.state.userPost.map((post) => {
+    return  <Post
+            nome={post.nome}
+            foto={post.foto}
+            fotoPost={post.fotoPost}
+          />
+  })
+
+  return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'Ester'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-        <Post
-          nomeUsuario={'Davi '}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
-
-        <Post
-          nomeUsuario={'Theo'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
-        
+      {userPost}        
       </div>
     );
   }
--- semana5/insta4/src/components/Post/Post.js
+++ semana5/insta4/src/components/Post/Post.js
@@ -1,34 +1,36 @@
 import React from 'react'
 import './Post.css'
 
-import {IconeComContador} from '../IconeComContador/IconeComContador'
+import { IconeComContador } from '../IconeComContador/IconeComContador'
 
 import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
-import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import { SecaoComentario } from '../SecaoComentario/SecaoComentario'
 
 class Post extends React.Component {
   state = {
     curtido: false,
     numeroCurtidas: 0,
     comentando: false,
-    numeroComentarios: 0
+    numeroComentarios: 0,
+  
   }
+
 
   onClickCurtida = () => {
     console.log('Curtiu!')
     this.setState({
       curtido: !this.state.curtido
-   
+
     })
 
-    if (this.state.curtido){
+    if (this.state.curtido) {
       this.setState({
         numeroCurtidas: this.state.numeroCurtidas - 1
       })
     }
-    else{
+    else {
       this.setState({
         numeroCurtidas: this.state.numeroCurtidas + 1
       })
@@ -47,45 +49,57 @@ class Post extends React.Component {
       numeroComentarios: this.state.numeroComentarios + 1
     })
   }
-
   render() {
+
+    // Modifica o ícone da curtida
     let iconeCurtida
 
-    if(this.state.curtido) {
+    if (this.state.curtido) {
       iconeCurtida = iconeCoracaoPreto
     } else {
       iconeCurtida = iconeCoracaoBranco
     }
 
+    // Modifica o comentário
     let componenteComentario
 
-    if(this.state.comentando) {
-      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    if (this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario} />
     }
 
-    return <div className={'post-container'}>
-      <div className={'post-header'}>
-        <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
-        <p>{this.props.nomeUsuario}</p>
-      </div>
+    // Mapeando o array de informações
+    return (
+      <div className={'post-container'}>
+        
+        <div className={'post-header'}>
+          <img className={'user-photo'} src={this.props.foto} alt={'Imagem do usuario'}/>
+          <p>{this.props.nome}</p>
+        </div>
 
-      <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
+        <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
 
-      <div className={'post-footer'}>
-        <IconeComContador
-          icone={iconeCurtida}
-          onClickIcone={this.onClickCurtida}
-          valorContador={this.state.numeroCurtidas}
-        />
+        <div className={'post-footer'}>
 
-        <IconeComContador
-          icone={iconeComentario}
-          onClickIcone={this.onClickComentario}
-          valorContador={this.state.numeroComentarios}
-        />
-      </div>
-      {componenteComentario}
-    </div>
+          <IconeComContador
+            icone={iconeCurtida}
+            onClickIcone={this.onClickCurtida}
+            valorContador={this.state.numeroCurtidas}
+          />
+
+          <IconeComContador
+            icone={iconeComentario}
+            onClickIcone={this.onClickComentario}
+            valorContador={this.state.numeroComentarios}
+          />
+
+        </div>
+
+        {componenteComentario}
+      
+      </div>          
+    );
+ 
+
   }
 }
 
