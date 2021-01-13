a98181a01f80074a17b93271031c85fc6d3f656b
willfjr <willferreirajr@icloud.com>
2021-01-12 23:03:13 -0300

    Renderização condicional realizada

--- semana7/labe-users/src/App.js
+++ semana7/labe-users/src/App.js
@@ -1,9 +1,7 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import InputBox from './components/InputBox';
 import axios from 'axios';
-
 
 function App() {
 
@@ -11,94 +9,126 @@ function App() {
 
   const [inputName, setInputName] = useState('');
 
-    function handleName(newName) {
-      setInputName(newName);
-    }
-  
+  function handleName(newName) {
+    setInputName(newName);
+  }
+
   const [inputEmail, setInputEmail] = useState('');
 
-    function handleEmail(newEmail) {
-      setInputEmail(newEmail);
-    }
+  function handleEmail(newEmail) {
+    setInputEmail(newEmail);
+  }
 
   const showAllUsers = () => {
+    
     axios
-        .get(
-          "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-          {
-            headers:{
-              Authorization: "wilson-ferreira-epps"
-            }
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+        {
+          headers: {
+            Authorization: "wilson-ferreira-epps"
           }
-        )
-        .then((res) => {
-          setUserList(res.data)
-        })
-        .catch((error) => {
-          alert('Não foi possível mostrar a lista')
-        });
-    }
+        }
+      )
+      .then((res) => {
+        setUserList(res.data)
+        alert('Tudo certo!')
+      })
+      .catch((error) => {
+        alert('Não foi possível mostrar a lista')
+      });
+  }
 
   const createUser = () => {
-      const body = {
-        name: inputName,
-        email: inputEmail
-      };
+    const body = {
+      name: inputName,
+      email: inputEmail
+    };
 
-      axios
-        .post(
-          "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-          body,
-          {
-            headers:{
-              Authorization: "wilson-ferreira-epps"
-            }
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+        body,
+        {
+          headers: {
+            Authorization: "wilson-ferreira-epps"
           }
-        )
-        .then((res) => {
-          setInputName("");
-          setInputEmail("");
-          alert('Uhulll')
-        })
-        .catch((error) => {
-          console.log(body)
-          alert('Não foi possível salvar seus dados')
-        });
+        }
+      )
+      .then((res) => {
+        setInputName("");
+        setInputEmail("");
+        alert('Uhulll');
+      })
+      .catch((error) => {
+        alert('Não foi possível salvar seus dados')
+      });
+  }
+
+  console.log(userList)
+
+  const [pageList, setPageList] = useState(false)
+
+  useEffect(() => {
+    if (pageList === true){
+     showAllUsers()
     }
-    
-    console.log(userList)
+  }, [pageList]); 
+  
+  let changeToPageList = () => {
+    setPageList(true);
+  }
 
-  return (
-    
-    <div className="App">
-      
-      <h1> Epps - Cadastro </h1>
-      
-      <p>
-        <InputBox 
-          frase="Digite seu nome"
-          onChangeText={handleName}
-        />
-      </p>
+  let returnToHome = () => {
+    setPageList(false)
+  }
 
-      <p>
-        <InputBox 
-         frase="Digite seu e-mail"
-         onChangeText={handleEmail}
-        />
-      </p>
+  // Pega os nomes
+  const nameList = userList.map(n => {
+    return <p>{n.name}</p>
+  })
 
-       <button onClick={createUser}>Adicionar</button>
-      <p>
-      <button onClick={showAllUsers}>Mostrar Usuários</button>
-      </p>
+  if (pageList === false) {
 
-      <p>
-      
-      </p>
-    
-    </div>
-  );
+    return (
+      <div className="App">
+
+        <h1> Epps - Cadastro </h1>
+
+        <p>
+          <InputBox
+            frase="Digite seu nome"
+            onChangeText={handleName}
+          />
+        </p>
+
+        <p>
+          <InputBox
+            frase="Digite seu e-mail"
+            onChangeText={handleEmail}
+          />
+        </p>
+
+        <button onClick={createUser}>Adicionar</button>
+        <p>
+          <button onClick={changeToPageList}>Mostrar Usuários</button>
+        </p>
+
+      </div>
+    );
+  } else {
+
+    return (
+      <div className="App">
+        <p>
+         {nameList}
+        </p>
+        <p>
+          <button onClick={returnToHome}>Voltar</button>
+        </p>
+      </div>
+    )
+  };
 }
 
 export default App;
