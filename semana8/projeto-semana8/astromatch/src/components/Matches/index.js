151710000f5cc02d8433e21ec655f6c2a9d372ce
willfjr <willferreirajr@icloud.com>
2021-01-29 17:34:45 -0300

    Função de pegar os Matches resolvidagit add .! Uhuulll...falta apenas o css para deixar bonito.

--- semana8/projeto-semana8/astromatch/src/components/Matches/index.js
+++ semana8/projeto-semana8/astromatch/src/components/Matches/index.js
@@ -3,17 +3,19 @@ import MatchesList from './styled';
 
 
 export const Matches = (props) => {
-         console.log(props.list.matches)  
-
-
     return (
-    <>{props.person &&         
-        <MatchesList>
-            {/* <img src={props.list.photo} alt={props.list.name}/>
-            <h2>{props.list.name}</h2> */}
-        </MatchesList>}
-    </>
+        <>{props.list &&         
+            <MatchesList>
+                {props.list && props.list.matches && props.list.matches.map((n) => {
+            
+                return <p>{n.name}</p>})}
+            </MatchesList>}
+        </>
     );
 }
-
 export default Matches;
+
+
+   {/* <img src={props.list.photo} alt={props.list.name}/>
+                    <h2>{props.list.name}</h2> */}
+
