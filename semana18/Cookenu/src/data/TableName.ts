0f5123c8c91554e42994a4f260509addf16bed20
unknown <willferreirajr@icloud.com>
2021-04-13 00:46:39 -0300

    Cria endpoint signUp

--- semana18/Cookenu/src/data/TableName.ts
+++ semana18/Cookenu/src/data/TableName.ts
@@ -1,4 +1,4 @@
-import { recipe } from "./../types";
+
 export const userTableName = "cookenu_users";
 
 export const recipeTableName = "cookenu_recipes";
--- semana18/Cookenu/src/index.ts
+++ semana18/Cookenu/src/index.ts
@@ -1,2 +1,10 @@
 import app from "./app";
-import { generateId } from "./services/idGenerator";
+import { signUp } from "./endpoints/users/signup";
+
+app.post("/users/signup", signUp);
+app.post("/users/login");
+app.get("/users/profile");
+app.get("users/:id/profile");
+
+app.post("recipe");
+app.get("/recipe/:id");
